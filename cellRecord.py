3b76c27686f7925fc94fe4c2e3f8818adedc5c88
xiaoxiao <13202405349@163.com>
2018-01-09 02:55:11 +0800

    use with to open file and get the json object ,after we can update the data such as new

--- cellRecord.py
+++ cellRecord.py
@@ -1,10 +1,14 @@
+import json
+records_str = ''
 try:
-    records_data = open("records")
-    for each_line in records_data:
-        try:
-            print(each_line)
-        except ValueError:
-            pass
-    records_data.close()
+    with open('records') as records_files:
+        for each_line in records_files:
+            records_str = records_str + each_line.strip()
+        print(records_str)
+        result_data = json.loads(records_str)
+        print(result_data)
+        for key in result_data.keys():
+            if result_data[key]['name'] == 'file':
+                print(result_data[key])
 except IOError:
     print("this file is missing")
